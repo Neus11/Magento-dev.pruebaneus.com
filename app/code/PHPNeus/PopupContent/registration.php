55a4c45f90f83e04144b96917819e2b63019b669
admin@example.com <admin@debian>
2022-06-09 14:50:17 +0200

    Shipping + Popup

---
+++ app/code/PHPNeus/PopupContent/registration.php
@@ -0,0 +1,8 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'PHPNeus_PopupContent',
+        __DIR__
+    );
+
+?>
