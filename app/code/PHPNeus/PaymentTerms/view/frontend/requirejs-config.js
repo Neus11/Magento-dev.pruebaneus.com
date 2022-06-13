e8a89e0d5326bde2db584982eef044c16d921b01
admin@example.com <admin@debian>
2022-06-13 12:09:05 +0200

    CHeckOut & HotDeals

---
+++ app/code/PHPNeus/PaymentTerms/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+
+ var config = {
+    // These codes helps us to override the knockout HTML template file.
+    map: {
+        '*': {
+            'Magento_CheckoutAgreements/template/checkout/checkout-agreements.html': 'PHPNeus_PaymentTerms/template/checkout/checkout-agreements.html'
+        }
+    }
+};
