bcbe8abd06e5a9ea02e623a9562509298150f73f
admin@example.com <admin@debian>
2022-06-14 12:24:21 +0200

    UrlRewrite

---
+++ app/code/PHPNeus/UrlRewrite/registration.php
@@ -0,0 +1,8 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'PHPNeus_UrlRewrite',
+        __DIR__
+    );
+
+?>
