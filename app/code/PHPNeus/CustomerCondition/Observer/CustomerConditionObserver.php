d3160bec2579737fce92ff2e5565c6952c23975d
Neus <75839031+Neus11@users.noreply.github.com>
2022-06-16 12:20:29 +0200

    Add files via upload

---
+++ app/code/PHPNeus/CustomerCondition/Observer/CustomerConditionObserver.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace PHPNeus\CustomerCondition\Observer;
+
+class CustomerConditionObserver implements \Magento\Framework\Event\ObserverInterface
+{
+    /**
+     * Execute observer.
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $additional = $observer->getAdditional();
+        $conditions = (array) $additional->getConditions();
+
+        // Merging the old condition with our condition.
+        $conditions = array_merge_recursive($conditions, [
+            [
+                'value'=> [
+                    [
+                        'value' => \PHPNeus\CustomerCondition\Model\Rule\Condition\Customer\Gender::class,
+                        'label' => __('Gender')
+                    ]
+                ],
+                'label'=> __('Customer Condition')
+            ]
+        ]);
+
+        $additional->setConditions($conditions);
+        return $this;
+    }
+}
